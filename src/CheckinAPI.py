ef72254c75c6140df8d671f63f1ded3355395fa0
Jonny Le <jonnyle2@gmail.com>
2025-06-17 11:26:09 -0500

    hotfix: update initial 1 hour check args

--- src/CheckinAPI.py
+++ src/CheckinAPI.py
@@ -64,7 +64,7 @@ CRONJOB_24_CHECKS = CronTrigger.from_crontab(os.environ['CRONJOB_24_CHECKS'])
 CRONJOB_1_CHECKS = CronTrigger.from_crontab(os.environ['CRONJOB_1_CHECKS'])
 scheduler = BackgroundScheduler()
 # schedule 1 hour calls inbetween deployment time and next scheduled 1 hour pre-calls (+1 minute to include any at cronjob time)
-check_in.schedule_1_hour_checks(scheduler, report, sms_controller, smartsheet_controller, CRONJOB_1_CHECKS.get_next_fire_time(None, datetime.now(timezone.utc)) + timedelta(minutes=1))
+check_in.schedule_1_hour_checks(scheduler, smartsheet_controller, SMARTSHEET_REPORT_ID, geolocator, sms_controller, CRONJOB_1_CHECKS.get_next_fire_time(None, datetime.now(timezone.utc)) + timedelta(minutes=1))
 # add 24 and 1 hour check jobs using crontab expression
 cron_24hr_job = scheduler.add_job(check_in.send_24_hour_checks, CRONJOB_24_CHECKS, args=[smartsheet_controller, SMARTSHEET_REPORT_ID, f'{N8N_BASE_URL}/{N8N_WORKFLOW_ID}', sms_controller, geolocator])
 cron_1hr_job = scheduler.add_job(check_in.schedule_1_hour_checks, CRONJOB_1_CHECKS, args=[scheduler, smartsheet_controller, SMARTSHEET_REPORT_ID, geolocator, sms_controller])
